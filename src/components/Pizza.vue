0653632c0cf9b3be0bbc9e5c65617557bc305dbb
Aaron Gorder <aarongorder@gmail.com>
2021-05-13 20:11:52 -0600

    init

---
+++ src/components/Pizza.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card pizza">
+    <h1>Pizza</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pizza',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.pizza{
+height: 100em;
+width: 200em;
+}
+</style>
